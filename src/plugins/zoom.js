18faa2f8e41aa0a12242bb54b32630ee521ae455
Aswin Devarajan <aswin@I0272-Mac.local>
2017-12-12 17:20:49 +0530

    fixed: zoom error

--- src/plugins/zoom.js
+++ src/plugins/zoom.js
@@ -1,21 +1,21 @@
-let d3 = require('d3');
-/**
- * a plugin which helps in applying zoom functionality to following graphs
- * scatter chart, bar chart,
- */
-class Zoom{
-  constructor(){
-
-  }
-
-  scatterZoom(width, height){
-    let brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended),
-        idleTimeout,
-        idleDelay = 350;
-    
-  }
-}
-
-module.exports = zoomPlugin;
+// let d3 = require('d3');
+// /**
+//  * a plugin which helps in applying zoom functionality to following graphs
+//  * scatter chart, bar chart,
+//  */
+// class Zoom{
+//   constructor(){
+//
+//   }
+//
+//   scatterZoom(width, height){
+//     let brush = d3.brush().extent([[0, 0], [width, height]]).on("end", brushended),
+//         idleTimeout,
+//         idleDelay = 350;
+//
+//   }
+// }
+//
+// module.exports = zoomPlugin;
 
 // TODO:20 create the plugin for zoom functionality
